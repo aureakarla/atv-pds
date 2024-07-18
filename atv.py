380063dc1d62793cbdb83304c61e22e50715f361
aurea <aurea.b@escolar.ifrn.edu.br>
2024-07-18 08:10:44 -0300

    funções

---
+++ atv.py
@@ -0,0 +1,19 @@
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome      
+        self._idade = None          
+        self.__especies = None 
+
+    def get_especies(self):
+        return self.__especies
+
+    def set_especies(self, especies):
+        self.__especies = especies
+
+    def get_idade(self):
+        return self._idade
+
+    def set_idade(self, idade):
+        if idade > 0:
+            self._idade = idade
+
